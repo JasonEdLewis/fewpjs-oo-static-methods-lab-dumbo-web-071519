2b626234cfef3ebc719ba69842678ce218bc8fa9
jason <jasonedlewis@gmail.com>
2019-09-05 17:38:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  static sanitize (str){
+    srt1 = str.split('-').split(`${range(1,9)}`)
+  }
+  static titleize (str){
+
+  }
+
 }
